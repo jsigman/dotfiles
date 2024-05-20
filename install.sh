ba03e2bbcb8ec19e0c90c8b58c5acc63f6fafa45
John Sigman <john.sigman@gmail.com>
2024-05-20 15:32:39 -0400

    better install

--- install.sh
+++ install.sh
@@ -33,4 +33,30 @@ sudo systemctl isolate graphical.target
 # Enable dcvserver service
 sudo systemctl enable dcvserver
 
+if ! command -v docker >/dev/null 2>&1; then
+  echo "Installing docker"
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  # Update and install
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+# Install docker-compose
+if ! command -v docker-compose >/dev/null 2>&1; then
+  echo "Installing docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+
 echo "Installation and configuration completed."
