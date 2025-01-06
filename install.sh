553db4af2efe4ac81bea7d945aaa4d776e674467
John Sigman <john.sigman@gmail.com>
2025-01-06 16:06:04 -0500

    no docker

--- install.sh
+++ install.sh
@@ -88,34 +88,6 @@ sudo systemctl isolate graphical.target
 # Enable dcvserver service
 sudo systemctl enable dcvserver
 
-# Docker installation
-if ! command -v docker >/dev/null 2>&1; then
-    echo "Installing Docker..."
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] \
-        https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-        sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-fi
-
-# Install docker-compose
-if ! command -v docker-compose >/dev/null 2>&1; then
-    echo "Installing docker-compose..."
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-fi
-
-# Start Docker rootless
-systemctl --user start docker
-sudo loginctl enable-linger "$(whoami)"
-
 # File syncing
 OCAML_VERSION=5.2.0 ./install_unison.sh
 
